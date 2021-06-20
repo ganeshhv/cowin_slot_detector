2942102aba1b5ec3550b970fd40d7120cfc5925e
Ganesh H V <ganesh.hv@livnsense.com>
2021-06-20 13:46:31 +0530

    asset directory and utils

---
+++ lib/utils/styles/main_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainColors {
+  const MainColors();
+
+  static const Color mainColor = const Color(0xFF151C26);
+  static const Color secondColor = const Color(0xFFf4C10F);
+  static const Color titleColor = const Color(0xFF5a606b);
+  static const Color headerMenu = const Color(0xFF343536);
+  static const Color childMenu = const Color(0xFF434446);
+  static const Color whiteBg = const Color(0xFFFFFFFF);
+  static const Color headerDashboardBg = const Color(0xFF2599E6);
+  static const Color mainDashboardBg = const Color(0xFFF5F5F5);
+  static const Color headerTableReport = const Color(0xFFDEDEDE);
+  static const Color dose1 = const Color(0xFF663B15);
+  static const Color dose2 = const Color(0xFF0051A8);
+  static const Color male = const Color(0xFF24585B);
+  static const Color female = const Color(0xFF992936);
+  static const Color aefi = const Color(0xFF56A638);
+  static const Color total = const Color(0xFF2C4E8B);
+}
+
+class MainTextStyle {
+  const MainTextStyle();
+
+  static const TextStyle defaultStyle1 = const TextStyle(
+    fontFamily: 'RobotoMono',
+    fontSize: 13,
+    color: Colors.white,
+  );
+
+  static const TextStyle defaultStyle2 = const TextStyle(
+    fontFamily: 'RobotoLight',
+    fontSize: 9,
+    color: Colors.white,
+  );
+  static const TextStyle defaultStyle3 = const TextStyle(
+    fontFamily: 'RobotoBold',
+    fontSize: 15,
+    color: Colors.black,
+  );
+}
